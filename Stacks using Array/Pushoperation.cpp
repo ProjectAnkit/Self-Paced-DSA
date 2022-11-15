944bb9198b37e26b4187698b733559cc10837a66
Ankit <xankitxthakurx@gmail.com>
2022-11-15 19:31:38 +0530

    Create Pushoperation.cpp

---
+++ Stacks using Array/Pushoperation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define MAX 6 
+using namespace std;
+
+class Stack{
+	public:
+	    void push(int data);
+	    void print();
+};
+
+int top = -1;
+int stack_arr[MAX];
+
+
+int main()
+{
+	Stack stack;
+	stack.push(2);
+	stack.push(4);
+	stack.push(5);
+	stack.push(4);
+	stack.push(8);
+	stack.push(9);
+	stack.push(10);
+	stack.print();
+	
+	return 0;
+}
+
+void Stack::push(int data)
+{
+	if(top == MAX-1)
+	{
+		cout<<"stack overflow"<<endl;
+		return;
+	}
+	
+	top = top+1;
+	stack_arr[top] = data;
+}
+
+void Stack::print()
+{
+	for(int i=0;i<=top;i++)
+	{
+		cout<<stack_arr[i]<<" ";
+	}
+}
