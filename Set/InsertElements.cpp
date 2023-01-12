44e2cdff1531273fc678776db7c097fad4d064ad
Ankit Thakur <xankitxthakurx@gmail.com>
2023-01-12 12:32:17 +0530

    Create InsertElements.cpp

---
+++ Set/InsertElements.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+
+int main()
+{
+   unordered_set<int>s;
+   s.insert(2);
+   s.insert(3);
+   s.insert(4);
+   
+   for(int i:s)
+   {
+   	  cout<<i<<endl;
+   }
+return 0;	
+}
